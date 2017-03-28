e1f8f62d868e144cba86651e57ef92ca848bf9e5
Evgeny Shulkin <evgeny.shulkin@gmail.com>
2017-03-28 12:03:56 +1000

    Remove handlebars template

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,15 +7,12 @@ $(document).ready(function() {
   const name = "Evgeny";
   // show message after 300ms
   setTimeout(() => alert(`Hello, ${name}!`), 300);
-  // use handlebars to print placeholders
-  var handlebars = require("handlebars");
-  // load helper to generate lorem ipsum text
-  handlebars.registerHelper("lorem", require("helper-lorem"));
-  var template = handlebars.compile("{{lorem this}}");
-  // place different size lorem text to main page elements
-  $("header").html(template({count: 20, units: "words"}));
-  $(".content").html(template({count: 3, units: "paragraphs"}));
-  $(".panel-left").html(template({count: 2, units: "paragraphs"}));
-  $(".panel-right").html(template({count: 1, units: "paragraphs"}));
-  $("footer").html(template({count: 10, units: "sentences"}));
+  // load module to generate lorem ipsum text
+  var lorem = require("lorem-ipsum");
+  // place different sized lorem placeholders to main page elements
+  $("header").html(lorem({count: 20, units: "words"}));
+  $(".content").html(lorem({count: 3, units: "paragraphs"}));
+  $(".panel-left").html(lorem({count: 2, units: "paragraphs"}));
+  $(".panel-right").html(lorem({count: 1, units: "paragraphs"}));
+  $("footer").html(lorem({count: 10, units: "sentences"}));
 });
