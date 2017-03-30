8bcf7d6118a3170472fd5e7cb005b19ae20623ca
Evgeny Shulkin <evgeny.shulkin@gmail.com>
2017-03-30 12:19:36 +1000

    Make flexbox layout fullscreen

--- assets/js/app.js
+++ assets/js/app.js
@@ -12,12 +12,14 @@ $(document).ready(function() {
   var lorem = require("lorem-ipsum");
   // place different sized lorem placeholders to main page elements
   $("header").html(lorem({count: 5, units: "sentences"}));
+  $(".panel-left").html(lorem({count: 10, units: "paragraphs"}));
   $(".content").html(lorem({count: 20, units: "paragraphs"}));
-  $("#layers-list").html(lorem({count: 5, units: "paragraphs"}));
-  $("#tools").html(lorem({count: 5, units: "paragraphs"}));
+  //$("#layers-list").html(lorem({count: 5, units: "paragraphs"}));
+  //$("#tools").html(lorem({count: 5, units: "paragraphs"}));
   $(".panel-right").html(lorem({count: 10, units: "paragraphs"}));
   $("footer").html(lorem({count: 7, units: "sentences"}));
   //--
+  /*
   require("jquery-resizable-dom");
   $("#layers-list").resizable({
     handleSelector: "#sp-layers",
@@ -27,6 +29,7 @@ $(document).ready(function() {
     handleSelector: "#sp-left",
     resizeHeight: false
   });
+  */
   //--
   // create mouse handlers for resize spliters
   /*
