251455519389d0b4290fd429c35cea6493f70849
Evgeny Shulkin <evgeny.shulkin@gmail.com>
2017-03-30 14:04:05 +1000

    Limit container height on fullscreen layout

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,14 +11,14 @@ $(document).ready(function() {
   // load module to generate lorem ipsum text
   var lorem = require("lorem-ipsum");
   // place different sized lorem placeholders to main page elements
-  $("header").html(lorem({count: 5, units: "sentences"}));
+  $("header").html(lorem({count: 5, units: "paragraphs"}));
   $(".panel-left").html(lorem({count: 10, units: "paragraphs"}));
   $(".content").html(lorem({count: 20, units: "paragraphs"}));
-  //$("#layers-list").html(lorem({count: 5, units: "paragraphs"}));
-  //$("#tools").html(lorem({count: 5, units: "paragraphs"}));
   $(".panel-right").html(lorem({count: 10, units: "paragraphs"}));
-  $("footer").html(lorem({count: 7, units: "sentences"}));
-  //--
+  $("footer").html(lorem({count: 7, units: "paragraphs"}));
+  /* ======================================================== */
+  /* OUTDATED */
+  /* ======================================================== */
   /*
   require("jquery-resizable-dom");
   $("#layers-list").resizable({
@@ -30,6 +30,8 @@ $(document).ready(function() {
     resizeHeight: false
   });
   */
+  //$("#layers-list").html(lorem({count: 5, units: "paragraphs"}));
+  //$("#tools").html(lorem({count: 5, units: "paragraphs"}));
   //--
   // create mouse handlers for resize spliters
   /*
