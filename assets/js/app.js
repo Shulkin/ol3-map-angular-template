0cd62cc8163597b96685ddf1f2bc76072b77a4fb
Evgeny Shulkin <evgeny.shulkin@gmail.com>
2017-04-05 15:09:18 +1000

    Delete comments

--- assets/js/app.js
+++ assets/js/app.js
@@ -20,16 +20,11 @@ $(document).ready(function() {
     {target: $("#map"), preset: "medium"},
     {target: $("#legend"), preset: "medium"},
     {target: $("footer"), preset: "medium"}
-    /*
-    {target: $("header"), preset: "medium"},
-    {target: $("#layers-list"), preset: "big"},
-    {target: $("#tools"), preset: "big"},
-    {target: $(".content"), preset: "big"},
-    {target: $(".panel-right"), preset: "medium"},
-    {target: $("footer"), preset: "medium"}
-    */
   ]);
-  // ===
+  /*
+   * Use small jQuery plugin by Rick Strahl to make DOM elements resizable
+   * More here: https://github.com/RickStrahl/jquery-resizable
+   */
   require("jquery-resizable-dom");
   $(".left").resizable({
     handleSelector: "#sp-left",
@@ -45,40 +40,4 @@ $(document).ready(function() {
     handleSelector: "#sp-layers",
     resizeWidth: false
   });
-  // ===
-  /*
-   * Use small jQuery plugin by Rick Strahl to make DOM elements resizable
-   * More here: https://github.com/RickStrahl/jquery-resizable
-   */
-  //require("jquery-resizable-dom");
-  // define onDrag callback
-  /*
-  var onDrag = function (e, $el, newWidth, newHeight, opt) {
-    if ($(".content").width() <= 118) {
-      if (newWidth < $el.width()) {
-        $el.width(newWidth);
-      } else {
-        return false;
-      }
-    }
-  };
-  */
-  /*
-  // side panels resizable
-  $(".panel-left").resizable({
-    handleSelector: "#sp-left",
-    resizeWidthFrom: "right",
-    resizeHeight: false
-  });
-  $(".panel-right").resizable({
-    handleSelector: "#sp-right",
-    resizeWidthFrom: "left",
-    resizeHeight: false
-  });
-  // content in left panel resizable
-  $("#layers-list").resizable({
-    handleSelector: "#sp-layers",
-    resizeWidth: false
-  });
-  */
 });
