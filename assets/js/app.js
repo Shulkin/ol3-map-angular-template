aaa06879d8256a646dbc1df680982c2eb385b255
Evgeny Shulkin <evgeny.shulkin@gmail.com>
2017-04-05 13:53:18 +1000

    Create new layout to resize elements

--- assets/js/app.js
+++ assets/js/app.js
@@ -15,17 +15,37 @@ $(document).ready(function() {
   var debug = require("./debug.js");
   debug.generateLorem([
     {target: $("header"), preset: "medium"},
+    {target: $(".left"), preset: "medium"},
+    {target: $("#map"), preset: "medium"},
+    {target: $("#legend"), preset: "medium"},
+    {target: $("footer"), preset: "medium"}
+    /*
+    {target: $("header"), preset: "medium"},
     {target: $("#layers-list"), preset: "big"},
     {target: $("#tools"), preset: "big"},
     {target: $(".content"), preset: "big"},
     {target: $(".panel-right"), preset: "medium"},
     {target: $("footer"), preset: "medium"}
+    */
   ]);
+  // ===
+  require("jquery-resizable-dom");
+  $(".left").resizable({
+    handleSelector: "#sp-left",
+    resizeWidthFrom: "right",
+    resizeHeight: false
+  });
+  $("#legend").resizable({
+    handleSelector: "#sp-legend",
+    resizeWidthFrom: "left",
+    resizeHeight: false
+  });
+  // ===
   /*
    * Use small jQuery plugin by Rick Strahl to make DOM elements resizable
    * More here: https://github.com/RickStrahl/jquery-resizable
    */
-  // require("jquery-resizable-dom");
+  //require("jquery-resizable-dom");
   // define onDrag callback
   /*
   var onDrag = function (e, $el, newWidth, newHeight, opt) {
