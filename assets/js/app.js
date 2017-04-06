cf4cea249bea8bc8198d0a6347f78e4a7fb4c96e
Evgeny Shulkin <evgeny.shulkin@gmail.com>
2017-04-06 13:15:33 +1000

    Add hacks to improve usability on resize

--- assets/js/app.js
+++ assets/js/app.js
@@ -21,6 +21,10 @@ $(document).ready(function() {
     {target: $("#legend"), preset: "medium"},
     {target: $("footer"), preset: "medium"}
   ]);
+  // utility function
+  var resetWidth = function(target) {
+    target.width(target.width());
+  };
   /*
    * Use small jQuery plugin by Rick Strahl to make DOM elements resizable
    * More here: https://github.com/RickStrahl/jquery-resizable
@@ -30,28 +34,34 @@ $(document).ready(function() {
     handleSelector: "#sp-left",
     resizeWidthFrom: "right",
     resizeHeight: false,
+    /*
+     * Two optional hacks
+     * Slightly increases usability when resizing
+     */
     onDrag: function(e, $el, newWidth, newHeight, opt) {
-      if ($(".content").width() <= 118) {
-        if (newWidth < $el.width()) {
-          return false;
-        }
-      }
+      resetWidth($("#legend"));
+    },
+    onDragEnd: function(e, $el, opt) {
+      resetWidth($el);
     }
   });
   $("#legend").resizable({
     handleSelector: "#sp-legend",
     resizeWidthFrom: "left",
     resizeHeight: false,
-    onDrag: function(e, $el, newWidth, newHeight, opt) {
-      if ($(".content").width() <= 118) {
-        if (newWidth < $el.width()) {
-          return false;
-        }
-      }
+    /* The same hack here */
+    onDragEnd: function(e, $el, opt) {
+      resetWidth($el);
     }
   });
   $("#layers-list").resizable({
     handleSelector: "#sp-layers",
     resizeWidth: false
+  });
+  /* Third hack */
+  window.addEventListener("resize", function(event) {
+    // prevents side panels from changing width on resize
+    resetWidth($("#legend"));
+    resetWidth($(".left"));
   });
 });
